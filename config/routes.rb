752d6ed693cdbb5a4a6c4bc63e25d80d673cba8d
Chris Simmons <chris@turing.edu>
2023-07-05 13:35:29 -0400

    Adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :pies do
+    resources :ingredients, only: [:index]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :cakes do
+    resources :ingredients, only: [:index]
+  end
+
+  resources :bakers, only: [:index] do
+    resources :recipes, only: [:index]
+  end
+
 end
